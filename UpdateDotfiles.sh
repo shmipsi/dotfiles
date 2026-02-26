f63a24f4fb27de08ee3d3cc27381ef0f31726a0a
shmipsi <shmipsi@proton.me>
2026-02-27 00:09:39 +0200

    ud script added is-else

--- UpdateDotfiles.sh
+++ UpdateDotfiles.sh
@@ -5,23 +5,38 @@ touch ~/files/dotfiles/.zshrc
 cat ~/.zshrc > ~/files/dotfiles/.zshrc
 
 # CONFIGS
-mkdir ~/files/dotfiles/.config
+if [ ! -d ~/files/dotfiles/.config ]; then
+	mkdir ~/files/dotfiles/.config
+	echo "created ~/files/dotfiles/.config"
+fi
 
 # kitty
-mkdir ~/files/dotfiles/.config/kitty
+if [ ! -d ~/files/dotfiles/.config/kitty ]; then
+	mkdir ~/files/dotfiles/.config/kitty
+	echo "created ~/files/dotfiles/.config/kitty"
+fi
 touch ~/files/dotfiles/.config/kitty/kitty.conf
 cat ~/.config/kitty/kitty.conf > ~/files/dotfiles/.config/kitty/kitty.conf
 
 # fastfetch
-mkdir ~/files/dotfiles/.config/fastfetch
+if [ ! -d ~/files/dotfiles/.config/fastfetch ]; then
+	mkdir ~/files/dotfiles/.config/fastfetch
+	echo "created ~/files/dotfiles/.config/fastfetch"
+fi
 touch ~/files/dotfiles/.config/fastfetch/config.jsonc
 touch ~/files/dotfiles/.config/fastfetch/logo.txt
 cat ~/.config/fastfetch/config.jsonc> ~/files/dotfiles/.config/fastfetch/config.jsonc
 cat ~/.config/fastfetch/logo.txt > ~/files/dotfiles/.config/fastfetch/logo.txt
 
 #code
-mkdir ~/files/dotfiles/.config/Code
-mkdir ~/files/dotfiles/.config/Code/User
+if [ ! -d ~/files/dotfiles/.config/Code ]; then
+	mkdir ~/files/dotfiles/.config/Code
+	echo "created ~/files/dotfiles/.config/Code"
+fi
+if [ ! -d ~/files/dotfiles/.config/Code/User ]; then
+	mkdir ~/files/dotfiles/.config/Code/User
+	echo "created ~/files/dotfiles/.config/Code/User"
+fi
 touch ~/files/dotfiles/.config/Code/User/settings.json
 cat ~/.config/Code/User/settings.json > ~/files/dotfiles/.config/Code/User/settings.json
 
